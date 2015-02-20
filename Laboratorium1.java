27b228e48f6973b63275fac7e50c6da94b8f7be0
Tomasz Kociński <kociak0@gmail.com>
2015-02-20 15:54:42 +0100

    dsadsa
    dsadas

---
+++ Laboratorium1.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.uksw.wmp.prja.laboratorium1;
+
+/**
+ *
+ * @author Kot
+ */
+public class Laboratorium1 {
+
+    public static double abs(double arg) {
+        if (arg < 0) {
+            arg = -arg;
+        }
+        return arg;
+    }
+
+    public static int silnia(int arg) {
+        int temp = 1;
+        for (int i = 1; i < arg + 1; i++) {
+            temp = temp * i;
+        }
+        return temp;
+    }
+
+    public static void fibo(int[] arg) {
+        if (arg.length > 0) {
+            arg[0] = 1;
+        }
+        if (arg.length > 1) {
+            arg[1] = 1;
+        }
+        for (int i = 2; i < arg.length; i++) {
+            arg[i] = arg[i - 2] + arg[i - 1];
+        }
+    }
+
+    public static void tablica(int[][] arg) {
+        for (int i = 0; i < arg.length; i++) {
+            for (int j = 0; j < arg[0].length; j++) {
+                arg[i][j] = (i + 2 + j) % 3;
+            }
+        }
+    }
+
+    public static double sin(double arg) {
+        double temp = 0;
+        for (int i = 1; i < 8; i++) {
+            double down,right = 1;
+            int up;
+            if ((i + 1) % 2 == 0) {
+                up = 1;
+            } else {
+                up = -1;
+            }
+            down = silnia(2* i - 1);
+            for (int j = 0; j < 2 * i - 1; j++) {
+                right = right * arg;
+            }
+            temp = temp + ((up / down) * right);
+            // temp=Math.floor(temp*1000000)/1000000;
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) { 
+        System.out.println(sin(Math.PI));
+        System.out.println(Math.sin(Math.PI)); // to jest strasznie mala wartosc, zapis naukowy (E-16), blisko zera
+        // 0.0
+        System.out.println(sin(0.0));
+        System.out.println(Math.sin(0.0));
+        // 0.0,
+        System.out.println(sin(Math.PI / 2.0));
+        System.out.println(Math.sin(Math.PI / 2.0));
+        //1.0
+        System.out.println(sin(0.5));
+        System.out.println(Math.sin(0.5));
+        // assertEquals(Math.sin(0.5), r, 0.000001);
+    }
+}
